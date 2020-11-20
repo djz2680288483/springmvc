c34f0d559e69f334bbc1fbb80c15861b8e114d6f
d_jiangzhou <2680288483@qq.com>
2020-11-20 20:02:05 +0800

    springmvc fourth commit

--- springmvcdemo/springmvc-annotation/src/main/java/com/djz/controller/HelloController.java
+++ springmvcdemo/springmvc-annotation/src/main/java/com/djz/controller/HelloController.java
@@ -1,8 +1,18 @@
 package com.djz.controller;
 
+import com.djz.entity.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.converter.ObjectToStringHttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author djz
@@ -17,4 +27,30 @@ public class HelloController {
         model.addAttribute("msg", "SpringMVC hello Test");
         return "hello";
     }
+
+    @GetMapping("/h2")
+    @ResponseBody
+    public String hello2() {
+
+        return "hello test";
+    }
+
+    @GetMapping("/h3")
+    @ResponseBody
+    public String hello3() throws JsonProcessingException {
+        User user=new User();
+        user.setName("lisi");
+        user.setAge(22);
+        user.setHobbys(Arrays.asList("sing","dance","run"));
+        Map<String,Object> map=new  HashMap();
+        map.put("身份证","52223199711210123");
+        map.put("银行卡","683748732210123");
+        user.setKey(map);
+
+      ObjectMapper objectMapper=new ObjectMapper();
+    String str=  objectMapper.writeValueAsString(user);
+        return str;
+    }
+
+
 }
---
+++ springmvcdemo/springmvc-annotation/src/main/java/com/djz/entity/User.java
@@ -0,0 +1,68 @@
+package com.djz.entity;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author djz
+ * @date 2020/11/20 -19:42
+ */
+public class User implements Serializable {
+    private String name;
+    private int age;
+    private List<String> hobbys;
+    private Map<String,Object> key;
+
+    public User() {
+    }
+
+    public User(String name, int age, List<String> hobbys, Map<String, Object> key) {
+        this.name = name;
+        this.age = age;
+        this.hobbys = hobbys;
+        this.key = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<String> getHobbys() {
+        return hobbys;
+    }
+
+    public void setHobbys(List<String> hobbys) {
+        this.hobbys = hobbys;
+    }
+
+    public Map<String, Object> getKey() {
+        return key;
+    }
+
+    public void setKey(Map<String, Object> key) {
+        this.key = key;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", hobbys=" + hobbys +
+                ", key=" + key +
+                '}';
+    }
+}
